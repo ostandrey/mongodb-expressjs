70527520c88cfafcbba81a1ea232a85ab5fe14c4
Andrew Ostapov <ost.and.g@gmail.com>
2020-12-04 18:44:02 +0200

    Reading data from the database

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/', adminController.getIndex);
 router.get('/add-anime', adminController.getAddAnime);
 router.post('/add-anime', adminController.postAnime);
+router.get('/:animeId', adminController.getAnime);
 
 module.exports = router;
